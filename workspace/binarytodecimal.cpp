258f4d79fa6942e89883967f5bc5de6c61bc350a
Imsujal16 <jaiswalsujal51@gmail.com>
2026-08-19 21:41:49 +0530

    Manual push: updated binarytodecimal.cpp

---
+++ workspace/binarytodecimal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int binarytodecimal(int n)
+{   int ans=0;
+    int i=0;
+    while(n>0)
+    {   
+        int ld=n%10;
+        if(ld==1)
+        {
+        ans+=pow(2,i); 
+        }
+        n/=10;
+        i++;
+    }
+    return ans;
+}
+int main() {
+    // Solution for binarytodecimal.cpp
+    int n=1010;
+   
+    cout <<binarytodecimal(n)<< endl;
+    return 0;
+}

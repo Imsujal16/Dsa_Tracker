05722da106bde63c85f807e2c93d8b71943f0c47
Imsujal16 <jaiswalsujal51@gmail.com>
2026-09-04 21:06:01 +0530

    Manual push: updated reversearray.cpp

---
+++ workspace/reversearray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Solution for reversearray.cpp
+    int arr[]={1,2,3,4,5};
+    for(int i=0, j=4; i<=j; i++,j--)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    for(int i=0; i<=4; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
